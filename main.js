bba8f89b5a82029549d311c420fe78e30457ad22
NazarovAbdulboriy <nazarovabdulboriy377@gmail.com>
2024-04-18 09:04:01 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let names = prompt("Ismingizni kiriting...");
+let id = prompt("IDni kiriting..");
+let password = prompt("Passwordni kiriting..");
+
+
+const marsit = {
+    name : names,
+    spaceID : id,
+    password : password,
+};
+
+console.log(marsit);
